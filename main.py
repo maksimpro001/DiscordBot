f96d37f523901040ca7acca6548724485f531e13
Max Muhanuk <maksimmuhanuk@gmail.com>
2023-10-04 21:08:47 +0300

    init git

---
+++ main.py
@@ -0,0 +1,13 @@
+# import discord
+# from discord.ext import commands
+#
+# bot = commands.Bot(command_prefix="/", intents=discord.Intents().all())
+#
+#
+# @bot.command()
+# async def hello(ctx):
+#     await ctx.reply("WhatsApp bro")
+#
+# bot.run(token="MTE1NzI3MzE0NDk5NDgyNDI4Mw.GLddO3.ta6R8fCqQg4ErS7qtBfrIU1FfGP2FvW5DL6mdc")
+
+print(int(str(int(input()))[::-1]))
