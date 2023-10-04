4cd1afe81ff29affea1688278ad363fbeade82d0
Max Muhanuk <maksimmuhanuk@gmail.com>
2023-10-04 21:22:00 +0300

    fix bags with init

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-# import discord
-# from discord.ext import commands
-#
-# bot = commands.Bot(command_prefix="/", intents=discord.Intents().all())
-#
-#
-# @bot.command()
-# async def hello(ctx):
-#     await ctx.reply("WhatsApp bro")
-#
-# bot.run(token="MTE1NzI3MzE0NDk5NDgyNDI4Mw.GLddO3.ta6R8fCqQg4ErS7qtBfrIU1FfGP2FvW5DL6mdc")
+import discord
+from discord.ext import commands
 
-print(int(str(int(input()))[::-1]))
+bot = commands.Bot(command_prefix="/", intents=discord.Intents().all())
+
+
+@bot.command()
+async def hello(ctx):
+    await ctx.reply("WhatsApp bro")
+
+bot.run(token="MTE1NzI3MzE0NDk5NDgyNDI4Mw.GLddO3.ta6R8fCqQg4ErS7qtBfrIU1FfGP2FvW5DL6mdc")
